7e142c6e4b08a9630e010a57e95e3951047636f8
aarshan <abdullahiaarshan@gmail.com>
2022-01-27 19:10:11 +0300

    Tailwind CSS Added

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1 class="text-2xl text-red-600 mx-6">Tailwind CSS</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
